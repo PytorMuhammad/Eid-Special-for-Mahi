87a96d0096c45deda85fbfb3aa07bcb23b57c32e
Py'tor <gmalyktoheed@gmail.com>
2025-09-10 14:57:08 +0500

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,9 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-app.use(express.static('.'));
+// Serve static files from current directory
+app.use(express.static(__dirname));
+app.use('/public', express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
